50b5169c09b61710a122673dba69f0df1c1fde40
yuanjianqing <497722789@qq.com>
2022-03-09 10:22:09 +0800

    123

--- 3/Code/Texture.hpp
+++ 3/Code/Texture.hpp
@@ -7,6 +7,11 @@
 #include "global.hpp"
 #include <eigen3/Eigen/Eigen>
 #include <opencv2/opencv.hpp>
+
+float lerp(float x, float y, float f)
+{
+    return (y - x) * f + x;
+}
 class Texture{
 private:
     cv::Mat image_data;
@@ -32,6 +37,40 @@ public:
         auto v_img = (1 - v) * height;
         auto color = image_data.at<cv::Vec3b>(v_img, u_img);
         return Eigen::Vector3f(color[0], color[1], color[2]);
+    }
+
+    //双线性纹理插值
+    Eigen::Vector3f getColorBilinear(float u, float v)
+    {
+        if (u < 0) u = 0;
+		if (u > 1) u = 1;
+		if (v < 0) v = 0;
+		if (v > 1) v = 1;
+        auto u_img = u * width;
+        auto v_img = (1 - v) * height;
+
+        u_img -= 0.5;
+        v_img -= 0.5;
+        Eigen::Vector3f v00 = {std::floor(u_img) + 0.5, std::floor(v_img)+ 0.5};
+        Eigen::Vector3f v10 = {std::ceil(u_img)+ 0.5, std::floor(v_img)+ 0.5};
+        Eigen::Vector3f v01 = {std::floor(u_img)+ 0.5, std::ceil(v_img)+ 0.5};
+        Eigen::Vector3f v11 = {std::ceil(u_img)+ 0.5, std::ceil(v_img)+ 0.5};
+
+        float w = v10.x() - (u_img + 0.5);
+        float h = v11.y() - (v_img + 0.5);
+
+        Eigen::Vector3f colorx1, colorx2, colory;
+        
+        auto color00 = image_data.at<cv::Vec3b>(v00.x(), v00.y());
+        auto color10 = image_data.at<cv::Vec3b>(v10.x(), v10.y());
+        auto color01 = image_data.at<cv::Vec3b>(v01.x(), v01.y());
+        auto color11 = image_data.at<cv::Vec3b>(v11.x(), v11.y());
+
+        
+        
+        auto color = image_data.at<cv::Vec3b>(v_img, u_img);
+        
+        
     }
 
 };
